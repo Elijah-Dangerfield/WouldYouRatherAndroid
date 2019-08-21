61b628fcd1055900806fddf0e2fb595939893e55
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-08-21 11:37:49 -0500

    adds navigation components and color resources

--- app/src/main/java/com/dangerfield/wouldyourather/MainActivity.kt
+++ app/src/main/java/com/dangerfield/wouldyourather/MainActivity.kt
@@ -2,11 +2,16 @@ package com.dangerfield.wouldyourather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
     }
 }
